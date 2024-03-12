8d03999aec016703ec4601a2cd531aeec095aa6b
Ray <rajancaccam@yahoo.com>
2024-03-12 18:46:53 -0400

    First Code Edit

--- .flaskenv
+++ .flaskenv
@@ -1,4 +1,4 @@
-export FLASK_APP=main.pyexport
-FLASK_DEBUG=1export
-FLASK_RUN_PORT=8080export
+export FLASK_APP=main.py
+FLASK_DEBUG=1
+FLASK_RUN_PORT=8080
 FLASK_RUN_HOST=0.0.0.0
--- main.py
+++ main.py
@@ -3,16 +3,21 @@ app = Flask(__name__)
 
 @app.route('/add/<num1>/<num2>')
 def add_nums(num1, num2):
-    return render_template('add.html',num1=num1,num2=num2)
+    res=str(int(num1)+int(num2))
+    return render_template('add.html',num1=num1,num2=num2,res=res)
 
 @app.route('/sub/<num1>/<num2>')
 def sub_nums(num1, num2):
-    return render_template('subtract.html',num1=num1,num2=num2)
+    res=str(int(num1)+int(num2))
+    return render_template('subtract.html',num1=num1,num2=num2,res=res)
 
 @app.route('/mult/<num1>/<num2>')
 def mult_nums(num1, num2):
-    return render_template('multiply.html',num1=num1,num2=num2)
+    res=str((num1)*int(num2))
+    return render_template('multiply.html',num1=num1,num2=num2,res=res)
 
 @app.route('/divd/<num1>/<num2>')
 def divd_nums(num1, num2):
-    return render_template('divide.html',num1=num1,num2=num2)
+    if num2 != 0:
+        res=str(float(num1)/int(num2))
+    return render_template('divide.html',num1=num1,num2=num2,res=res)
