af2356ffaa2e94e472db56c7e6a5d8593864b2e7
Ray <rajancaccam@yahoo.com>
2024-03-12 20:03:57 -0400

    Eighth Edit

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('home.html')
 @app.route('/add/<num1>/<num2>')
 def add_nums(num1, num2):
     if num1.isdigit()==num2.isdigit()==True:
-        res=str(int(num1)+int(num2))
+        res=str(float(num1)+float(num2))
         return render_template('add.html',num1=num1,num2=num2,res=res)
     else:
         return render_template('nonDigit.html')
@@ -12,7 +15,7 @@ def add_nums(num1, num2):
 @app.route('/sub/<num1>/<num2>')
 def sub_nums(num1, num2):
     if num1.isdigit()==num2.isdigit()==True:
-        res=str(int(num1)-int(num2))
+        res=str(float(num1)-float(num2))
         return render_template('subtract.html',num1=num1,num2=num2,res=res)
     else:
         return render_template('nonDigit.html')
@@ -20,7 +23,7 @@ def sub_nums(num1, num2):
 @app.route('/mult/<num1>/<num2>')
 def mult_nums(num1, num2):
     if num1.isdigit()==num2.isdigit()==True:
-        res=str(int(num1)*int(num2))
+        res=str(float(num1)*float(num2))
         return render_template('multiply.html',num1=num1,num2=num2,res=res)
     else:
         return render_template('nonDigit.html')
@@ -31,7 +34,7 @@ def divd_nums(num1, num2):
         if int(num2) == 0:
             res=num2
         else:
-            res=str(float(num1)/int(num2))
+            res=str(float(num1)/float(num2))
         return render_template('divide.html',num1=num1,num2=num2,res=res)
     else:
         return render_template('nonDigit.html')
